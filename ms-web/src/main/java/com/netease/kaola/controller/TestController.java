34fb269c7f158b91734d6da5d50f095796c12eec
hzwangqiqing <hzwangqiqing@corp.netease.com>
2017-08-16 10:50:47 +0800

    #增加web.xml-web-facets

--- ms-web/src/main/java/com/netease/kaola/controller/TestController.java
+++ ms-web/src/main/java/com/netease/kaola/controller/TestController.java
@@ -4,6 +4,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * Created by hzwangqiqing
  * on 2017/8/11.
@@ -16,4 +19,12 @@ public class TestController {
     public String page() {
         return "page";
     }
+
+
+    @RequestMapping("/page1")
+    @ResponseBody
+    public List<Long> page1() {
+        return Arrays.asList(new Long[]{1L, 2L, 3L});
+    }
+
 }
