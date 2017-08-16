a7ce7f46d63a47849a3ecd3d425372cfa647c37e
hzwangqiqing <hzwangqiqing@corp.netease.com>
2017-08-16 14:54:36 +0800

    #增加web.xml-web-facets

--- ms-web/src/main/java/com/netease/kaola/controller/TestController.java
+++ ms-web/src/main/java/com/netease/kaola/controller/TestController.java
@@ -1,11 +1,14 @@
 package com.netease.kaola.controller;
 
+import com.netease.kaola.vo.TestVO;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by hzwangqiqing
@@ -27,4 +30,21 @@ public class TestController {
         return Arrays.asList(new Long[]{1L, 2L, 3L});
     }
 
+    @RequestMapping("/page2")
+    @ResponseBody
+    public Map<String, Object> page2() {
+        Map<String, Object> map = new HashMap<>();
+        map.put("retCode", "200");
+        map.put("retDesc", "操作成功");
+        return map;
+    }
+
+    @RequestMapping("/page3")
+    @ResponseBody
+    public TestVO page3() {
+        TestVO testVO = new TestVO();
+        testVO.setId(1L);
+        testVO.setName("chaos");
+        return testVO;
+    }
 }
