f74f728f7f2bc359a4229a7ecc25ca9f876110d2
hzwangqiqing <hzwangqiqing@corp.netease.com>
2017-08-17 10:08:11 +0800

    #替换get/set-使用@data注解

--- ms-web/src/main/java/com/netease/kaola/vo/TestVO.java
+++ ms-web/src/main/java/com/netease/kaola/vo/TestVO.java
@@ -8,25 +8,10 @@ import java.io.Serializable;
  * Created by hzwangqiqing
  * on 2017/8/11.
  */
+@Data
 public class TestVO implements Serializable {
     private static final long serialVersionUID = 1233255437451371022L;
     private Long id;
 
     private String name;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 }
