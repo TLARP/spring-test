5124c5811a2859cbb9ff5edeafca5b7145e52401
hzwangqiqing <hzwangqiqing@corp.netease.com>
2017-10-13 15:30:28 +0800

    #添加一个本地rmi的例子

--- ms-web/src/main/java/com/netease/kaola/multithread/RunTest.java
+++ ms-web/src/main/java/com/netease/kaola/multithread/RunTest.java
@@ -6,11 +6,20 @@ package com.netease.kaola.multithread;
 public class RunTest implements Runnable {
     @Override
     public void run() {
-        System.out.println("thread is running!");
+        System.out.println("thread is running! and exit");
     }
 
     public static void main(String[] args) {
-        new Thread(new RunTest()).start();
-        System.exit(0);
+        Thread thread = new Thread(new RunTest());
+        thread.setDaemon(true);
+        thread.start();
+        try {
+            Thread.currentThread().sleep(5000L);
+        } catch (InterruptedException e) {
+            System.out.println(e);
+        }
+        while (true) {
+            System.out.println("hello world");
+        }
     }
 }
